f474df9f34b052bedbc8546260ec1937f7565f4f
Alex Pratte <alexpratte16@gmail.com>
2014-02-04 16:06:10 -0500

    BuildImageStreaming supports profiling mode

--- buildImageStreaming.sh
+++ buildImageStreaming.sh
@@ -6,8 +6,11 @@ if [ -z $CXX ]; then
                 CXX="/usr/bin/env g++"
         fi
 fi
+if [ $1 == 'profile' ]; then
+    PROFILING_FLAGS='-pg'
+fi
 if [ -d /usr/lib/arm-linux-gnueabi/ ]; then
 	EXTRA_FLAGS='-L/usr/lib/arm-linux-gnueabi/'
 fi
 #`pkg-config --cflags opencv` -lopencv_calib3d -lopencv_video -lopencv_contrib -lopencv_highgui -lopencv_gpu -lopencv_core -lopencv_ml -lopencv_features2d -lopencv_imgproc -lopencv_legacy -lopencv_flann -lopencv_objdetect
-$CXX --std=c++0x `pkg-config --cflags opencv` -g -o streamingMain -Wall -Werror streamingMain.cpp findCircle.cpp -ljpeg -pthread $EXTRA_FLAGS -lX11 `pkg-config --libs opencv`
+$CXX --std=c++0x `pkg-config --cflags opencv` -g $PROFILING_FLAGS -o streamingMain -Wall -Werror streamingMain.cpp findCircle.cpp -ljpeg -pthread $EXTRA_FLAGS -lX11 `pkg-config --libs opencv`
