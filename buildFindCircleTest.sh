473e404d511f8c9d9578664ee907e0c7ac9fc45d
Alex Pratte <alexpratte16@gmail.com>
2014-02-06 16:13:25 -0500

    Remove more junk

--- buildFindCircleTest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -z $CXX ]; then
-        if type clang++ >> /dev/null 2> /dev/null; then
-                CXX="/usr/bin/env clang++"
-        else
-                CXX="/usr/bin/env g++"
-        fi
-fi
-exec $CXX --std=c++0x -g -o findCircleTest -Wall -Werror `pkg-config --cflags --libs opencv` streamingMain.cpp findCircle.cpp -ljpeg 
